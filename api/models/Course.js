3e7a4149506ca53af9bb01e7e91e8dea906d582e
Martin Paladino <c.martin.pal@gmail.com>
2021-11-10 14:03:04 -0300

    complete modelo category y course

---
+++ api/models/Course.js
@@ -0,0 +1,50 @@
+const S = require("sequelize")
+const db = require("../config/db")
+
+class Course extends S.Model {}
+
+Course.init({
+    title: {
+        type: S.STRING,
+        allowNull: false
+    },
+    description: {
+        type: S.TEXT,
+        allowNull: false
+    },
+    professor: {
+        type: S.STRING,
+        allowNull: false
+    },
+    image: {
+        type: S.STRING,
+        allowNull: false
+    },
+    review: {
+        type: S.STRING,
+        allowNull: false
+    },
+    rating: {
+        type: S.DECIMAL(10, 1),
+        allowNull: false
+    },
+    price: {
+        type: S.DECIMAL(10, 2),
+        allowNull: false
+    },
+    duration: {
+        type: S.DECIMAL(10, 2),
+        allowNull: false
+    },
+    accessLink: {
+        type: S.STRING,
+        allowNull: false
+    },
+    videoPreview: {
+        type: S.STRING,
+        allowNull: false
+    }
+}, 
+{ sequelize: db, modelName: "course" })
+
+module.exports = Course
