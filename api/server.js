654aca8165aa2b40fc6971b050196dc4934a1133
benjacale <benjacale5@gmail.com>
2021-11-11 12:47:56 -0300

    configuracion de passport

--- api/server.js
+++ api/server.js
@@ -1,8 +1,49 @@
 const express = require("express")
+const morgan = require("morgan")
+const passport = require("passport");
+const session = require("express-session") // Permite guardar sesiones de los usuarios loggeados.
+const cookieParser  = require("cookie-parser");
 const db = require("./config/db")
 const models = require("./models")
+const routes = require("./routes")
+const localStrategy = require("./config/localStrategy");
+const { User }      = require("./models");
+
 
 const app = express()
+
+app.use(morgan("tiny"))
+app.use(express.json())
+
+// Parser que transforma de STR a OBJ la info contenida en una cookie.
+app.use(cookieParser());
+
+// Passport usará req.session para llevar un registro de quién está logueado en nuestra app.
+app.use( session({secret: "hello-world"}) ); 
+
+// Inicializamos Passport.
+app.use(passport.initialize());
+
+// Inicializamos el middleware de sesiones. Vinculamos la instancia de Passport con las sesiones configuradas en Express.
+app.use(passport.session());
+
+// Valida credenciales.
+passport.use(localStrategy);
+
+// Guardamos el id en la sesión (escribimos la cookie).
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+// Obtenemos el usuario a partir del id guardado (leemos la cookie).
+passport.deserializeUser(function(id, done) {
+    User.findByPk(id)
+    .then(user => done(null, user))
+    .catch(done);
+});
+
+
+app.use("/api", routes)
 
 db.sync({force: false})
 .then(() => {
