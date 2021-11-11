69d1c199dfd1c299215bd422f45969859a5238fa
Rodrigo de Sarasqueta <rod.nadaesnatural@gmail.com>
2021-11-11 15:06:12 -0300

    cart functionalityyyy

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,30 +10,34 @@ import { setUser } from "../state/user"
 // Tengo que comprobar si esta o no logueado usando Redux!
 
 const Login = () => {
-  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const [vari, setVari] = useState("light");
-
+  const dispatch = useDispatch();
+  
   function loguear(usr, pass) {
     let body = { "email": usr, "password": pass };
     setVari("primary")
     setMessage("Logueando...");
-    const dispatch = useDispatch();
+    
+///
 
-    axios
+
+    /* axios
       .post("/api/login", body)
-      .then((response) => dispatch(setUser(respose.data)))
+      .then((response) => dispatch(setUser(response.data)))
       .then((a) => {setVari("primary");setMessage("Que bueno volver a verte " + usr + ".")})
       .catch((e) => {
         setVari("danger");
         setMessage("Usuario o clave incorrecta.");
-      });
+      }); */
+
   }
   function handlerSubmit(e) {
     let comprobado =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        user
+        email
       );
 
     if (!comprobado) {
@@ -41,14 +45,13 @@ const Login = () => {
         setMessage("Correo invalido!");
     } else {
       if (password.length > 5) {
-        loguear(user, password);
+        loguear(email, password);
       } else {
         setVari("danger");
         setMessage("Password invalido.");
       }
     }
 
-    console.log("funciona", user);
   }
 
   return (
@@ -60,7 +63,7 @@ const Login = () => {
         <h2>Por favor, ingrese su usuario:</h2>
         <Form.Group>
           <Form.Control
-            onChange={(e) => setUser(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             type="email"
             size="lg"
             placeholder="Dirección de E-Mail"
