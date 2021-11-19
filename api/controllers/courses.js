8aa74143c206c24ba61a4bdf212f33b026e02736
G-Castagneri <gaston.castagneri@gmail.com>
2021-11-19 11:59:51 -0300

    search listo

--- api/controllers/courses.js
+++ api/controllers/courses.js
@@ -1,89 +1,97 @@
-const { Course, Category ,UserCourse} = require("../models")
-
+const { Course, Category, UserCourse } = require("../models");
+const { Op } = require("sequelize");
 class CoursesController {
+  static getAllCourses(req, res, next) {
+    Course.findAll()
+      .then((courses) => res.status(200).send(courses))
+      .catch(next);
+  }
 
-    static getAllCourses(req, res, next) {
-        Course.findAll()
-            .then(courses => res.status(200).send(courses))
-            .catch(next)
+  static checkIfPurchased(req, res, next) {
+    if (req.user) {
+      const courseId = parseInt(req.params.courseId);
+      UserCourse.findAll({
+        where: { userId: req.user.id, purchased: true },
+      }).then((userCourses) => {
+        const userCourse = userCourses.find(
+          (userCourse) => userCourse.courseId === courseId
+        );
+        res.status(200).send(userCourse ? true : false);
+      });
+    } else {
+      res.send(false);
     }
+  }
 
-    static checkIfPurchased(req, res, next) {
-        if(req.user)
-        {const courseId = parseInt(req.params.courseId)
-        UserCourse.findAll({ where: { userId: req.user.id, purchased: true } })
-            .then(userCourses => {
-                const userCourse = userCourses.find(userCourse => userCourse.courseId === courseId
-                )
-                res.status(200).send(userCourse ? true : false)
-            })}
-            else {
-                res.send(false)
-            }
-    }
+  static getCourse(req, res, next) {
+    Course.findOne({ where: { id: req.params.id } })
+      .then((course) => res.status(200).send(course))
+      .catch(next);
+  }
 
-    static getCourse(req, res, next) {
-        Course.findOne({ where: { id: req.params.id } })
-            .then(course => res.status(200).send(course))
-            .catch(next)
-    }
+  static getCoursesFromTitle(req, res, next) {
+    Course.findAll({ where: { title: req.params.courseTitle } })
 
-    static getCoursesFromTitle(req, res, next) {
-        Course.findAll({ where: { title: req.params.courseTitle } })
+      .then((courses) => res.status(200).send(courses))
+      .catch(next);
+  }
 
-            
-            .then(courses => res.status(200).send(courses) )
-            .catch(next)
-    }
+  static addCourse(req, res, next) {
+    Course.create(req.body)
+      .then((newCourse) => res.status(201).send(newCourse))
+      .catch(next);
+  }
 
-    static addCourse(req, res, next) {
-        Course.create(req.body)
-            .then(newCourse => res.status(201).send(newCourse))
-            .catch(next)
-    }
+  static updateCourse(req, res, next) {
+    Course.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+    })
+      .then((updatedCourse) => res.status(200).send(updatedCourse[1][0]))
+      .catch(next);
+  }
 
-    static updateCourse(req, res, next) {
-        Course.update(req.body, {
-            where: { id: req.params.id },
-            returning: true
-        })
-            .then(updatedCourse => res.status(200).send(updatedCourse[1][0]))
-            .catch(next)
-    }
+  static deleteCourse(req, res, next) {
+    Course.destroy({ where: { id: req.params.id } })
+      .then(() => res.sendStatus(202))
+      .catch(next);
+  }
 
-    static deleteCourse(req, res, next) {
-        Course.destroy({ where: { id: req.params.id } })
-            .then(() => res.sendStatus(202))
-            .catch(next)
-    }
+  static getCoursesFromCategory(req, res, next) {
+    Category.findAll({
+      where: { name: { [Op.like]: "%" + req.params.category + "%" } },
+    })
+      //Category.findOne({ where: { name: req.params.category }})
+      .then((categories) => {
+        const arrPromises = [];
+        for (let category of categories) {
+          arrPromises.push(category.getCourses());
+        }
+        return Promise.all(arrPromises);
+      })
+      .then((courses) => courses.flatMap((course) => course))
+      .then((courses) => res.status(200).send(courses))
+      .catch(next);
+  }
 
-    static getCoursesFromCategory(req, res, next) {
-        // Category.findOne({ where: {title:{ [Op.substring]: '%' + req.params.category + '%'}}}) no funciona ?????
-        Category.findOne({ where: { name: req.params.category }})
-        .then(category => category ? category.getCourses() : [])
-        .then(courses => res.status(200).send(courses))
-        .catch(next)
-    }
-
-    static addCategoryToCourse(req, res, next) {
-        Course.findOne({ where: { id: req.params.courseId } })
-            .then(course => {
-                return {
-                    course,
-                    categoryPromise: Category.findOne({ where: { id: req.params.categoryId } })
-                }
-            })
-            .then(({ course, categoryPromise }) => {
-                categoryPromise
-                    .then(category => {
-                        course.addCategory(category)
-                        res.sendStatus(201)
-                    })
-            })
-            .catch(next)
-    }
-
-
+  static addCategoryToCourse(req, res, next) {
+    Course.findOne({ where: { id: req.params.courseId } })
+      .then((course) => {
+        return {
+          course,
+          categoryPromise: Category.findOne({
+            where: { id: req.params.categoryId },
+          }),
+        };
+      })
+      .then(({ course, categoryPromise }) => {
+        categoryPromise.then((category) => {
+          course.addCategory(category);
+          res.sendStatus(201);
+        });
+      })
+      .catch(next);
+  }
 }
 
-module.exports = CoursesController
+module.exports = CoursesController;
