a8cf3fcc34211c2d1a8576c6810c21d0140415c7
LuisDurand1 <luis-rodrigo2213@hotmail.com>
2021-11-19 10:20:19 -0300

    correcion etiqueta div

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,41 +1,46 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "../commons/Card";
-import { getCoursesFromCategory, getAllCourses } from "../state/courses"
-import { Button , Container } from "react-bootstrap";
+import { getCoursesFromCategory, getAllCourses } from "../state/courses";
+import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-
-
 // estilos
-import "../assets/styles/general.css"
-import "../assets/styles/grid.css"
+import "../assets/styles/general.css";
+import "../assets/styles/grid.css";
 
 const Grid = () => {
   const dispatch = useDispatch();
-  const { category } = useParams()
-  const courses = useSelector(state => state.courses)
- 
+  const { category } = useParams();
+  const courses = useSelector((state) => state.courses);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    category ? dispatch(getCoursesFromCategory(category)) : dispatch(getAllCourses())
-  }, [category])
+    category
+      ? dispatch(getCoursesFromCategory(category))
+      : dispatch(getAllCourses());
+  }, [category]);
 
   return (
     <div>
-      <h1>Resultados para {category ? category : "Ver todos"}:</h1>
-    <div id="contGrid">
-      {courses.map(course =>
-      (<div id="espacio">
-        <div key={course.id}>
-          <Card key={course.id} course={course} />
-        </div>
-      </div>))}
-    </div>
-    </div>
-              <Button className="boton" onClick={() =>navigate(-1)}> Volver</Button>
+      <div>
+        <h1>Resultados para {category ? category : "Ver todos"}:</h1>
+        <div id="contGrid">
+          {courses.map((course) => (
+            <div id="espacio">
+              <div key={course.id}>
+                <Card key={course.id} course={course} />
               </div>
-  )
+            </div>
+          ))}
+        </div>
+      </div>
+      <Button className="boton" onClick={() => navigate(-1)}>
+        Volver
+      </Button>
+    </div>
+  );
 };
 
 export default Grid;
